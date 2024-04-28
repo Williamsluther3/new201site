d43c80dd9534f8631c9ef7bf12234f5d54022bc5
Williamsluther3 <williamsluther3@gmail.com>
2024-04-27 23:12:51 -0500

    second add to 201 lab

--- Js/app.js
+++ Js/app.js
@@ -19,37 +19,94 @@ function greetUser(){
 
 }
 
-greetUser();
-
-const knowLuther = prompt('Do you know Luther Williams?');
-
-
-let text = knowLuther.toUpperCase();
+//greetUser();
+codeFellows();
+currentJob();
 
 
-if (knowLuther === 'YES'){
-  console.log('YES');
+let knowLuther = prompt('Do you know Luther Williams?');
+knowLuther = knowLuther.toUpperCase();
+console.log(knowLuther);
 
+
+// let really = prompt('Your answer was ' + knowLuther + ' Is this correct?');
+// really = really.toUpperCase();
+// console.log(really);
+
+if (knowLuther == 'NO' || knowLuther == 'N'){
+  alert('Oh No!!, We have to make sure you get to know him');
 }
-if (knowLuther === 'NO'){
-  console.log('NO');
 
+if (knowLuther == 'YES' || knowLuther == 'Y'){
+  alert("Awesome, he's a great guy");
+}
 
+let thoughtOnCodeFellows = prompt('Is code Fellows a good program?');
+thoughtOnCodeFellows = thoughtOnCodeFellows.toLowerCase();
+console.log(thoughtOnCodeFellows);
+
+if (thoughtOnCodeFellows == 'yes' || thoughtOnCodeFellows == 'y'){
+  alert('I agree, Code Fellows is a great program, KEPP Coding');
+}
+
+if (thoughtOnCodeFellows == 'no' || thoughtOnCodeFellows == 'n'){
+  alert("I'm sorry you feel that way, maybe give it another try");
 }
 
 
-function DoYouKnowLuther(knowLuther){
-  if (knowLuther === 'YES'){
-    alert("You don't say");
-    document.write('You answered  ' + knowLuther + ' Are his friend?');
+
+
+function codeFellows(thoughtOnCodeFellows){
+  if (thoughtOnCodeFellows == 'yes' || thoughtOnCodeFellows == 'y'){
+    console.log('codeFellows');
+
+    document.write('You like Code Fellows, YEeaahh Buddy, CodeFellows for life');
   }
-  if (knowLuther === 'NO'){
-    alert('You will after this');
-    document.write('Would you like to know alittle about him');
-  }
+  if (thoughtOnCodeFellows == 'no' || thoughtOnCodeFellows == 'n'){
+    console.log('codeFellows');
 
-  return knowLuther;
+    document.write("Shucks, You don't like Code Fellows, I though everyone liked Code Fellows");
+  }
 
 }
 
-console.log("Well let's find out");
+let currentJob = prompt('Do you like you currentjob position?');
+currentJob = currentJob.toLowerCase();
+console.log(currentJob);
+
+
+
+//alert("I'm glad you know")
+
+// if (knowLuther == 'YES'){
+//   console.log('YES');
+
+// }
+// if (knowLuther == 'NO'){
+//   console.log('NO');
+// }
+
+// function knowingHim(knowLuther){
+//   if (knowingHim == 'YES'){
+//     console.log('YES');
+//     alert("You don't say");
+//     document.write('Are you two friend?');
+//   }
+//   if (knowingHim == 'NO'){
+//     console.log('NO');
+//     alert('You will after this');
+//     document.write('Would you like to know alittle about him');
+//   }
+//   else {
+//     console.log("You don't know me man!!!");
+//   }
+
+// knowingHim();
+// really();
+
+
+//}
+
+//console.log("Well let's find out");
+
+//doYouKnowLuther();
